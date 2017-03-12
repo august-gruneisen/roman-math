546d30034290b58a66203bfed41e9cf6d2cac744
unknown <a.gruneisen@gmail.com>
2017-03-12 01:32:08 -0800

    Works to 3999

--- src/Roman_Math.java
+++ src/Roman_Math.java
@@ -6,12 +6,7 @@ import javax.swing.JOptionPane;
 
 public class Roman_Math {
 	public static void main(String args []) {
-		JOptionPane.showMessageDialog(null, "another");
-	}
-	
-	public String tester() {
-		String i = new String("II");
-		return i;
+		JOptionPane.showMessageDialog(null, "running");
 	}
 	
 	public String IplusI() {
@@ -117,13 +112,18 @@ public class Roman_Math {
 	
 	public String toRomans(String number) {
 		String output = number;
-		output = toIV(output);
-		output = toV(output);
-		output = toIX(output);
-		output = toX(output);
-		
 		output = output.replace("IIIII", "V");
-		
+		output = output.replace("VV", "X");
+		output = output.replace("XXXXX", "L");
+		output = output.replace("LL", "C");
+		output = output.replace("CCCCC", "D");
+		output = output.replace("DD", "M");
+		output = output.replace("DCCCC", "CM");
+		output = output.replace("CCCC", "CD");
+		output = output.replace("LXXXX", "XC");
+		output = output.replace("XXXX", "XL");
+		output = output.replace("VIIII", "IX");
+		output = output.replace("IIII", "IV");
 		return output;
 	}
 
@@ -164,10 +164,18 @@ public class Roman_Math {
 
 	public String toIs(String number) {
 		String output = number;
-		output = toIIII(output);
-		output = toIIIII(output);
+		output = output.replace("IV", "IIII");
+		output = output.replace("IX", "VIIII");
+		output = output.replace("XL", "XXXX");
+		output = output.replace("XC", "LXXXX");
+		output = output.replace("CD", "CCCC");
+		output = output.replace("CM", "DCCCC");
+		output = output.replace("M", "DD");
+		output = output.replace("D", "CCCCC");
+		output = output.replace("C", "LL");
+		output = output.replace("L", "XXXXX");
+		output = output.replace("X", "VV");
 		output = output.replace("V", "IIIII");
-		
 		return output;
 	}
 
@@ -270,5 +278,359 @@ public class Roman_Math {
 		
 		return result;
 	}
+
+	public String VplusVI() {
+		String first = "V";
+		String second = "VI";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String VIIIplusVI() {
+		String first = "VIII";
+		String second = "VI";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String VIIIplusVIII() {
+		String first = "VIII";
+		String second = "VIII";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String IXplusI() {
+		String first = "IX";
+		String second = "I";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String toIIIIIIIII(String number) {
+		String output = number;
+		if (output.equals("IX")) {
+			output = "IIIIIIIII";
+		}
+		return output;
+	}
+	
+	public String VIIIplusX() {
+		String first = "VIII";
+		String second = "X";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String toIIIIIIIIII(String number) {
+		String output = number;
+		if (output.equals("X")) {
+			output = "IIIIIIIIII";
+		}
+		return output;
+	}
+
+	public String XplusIX() {
+		String first = "X";
+		String second = "IX";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String XplusX() {
+		String first = "X";
+		String second = "X";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String XIplusIV() {
+		String first = "XI";
+		String second = "IV";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+
+	public String XIplusV() {
+		String first = "XI";
+		String second = "V";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String VIIIplusXIII() {
+		String first = "VIII";
+		String second = "XIII";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String XIIIplusIX() {
+		String first = "XIII";
+		String second = "IX";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+
+	public String XIplusXIII() {
+		String first = "XI";
+		String second = "XIII";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+
+	public String XIIplusXIII() {
+		String first = "XII";
+		String second = "XIII";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+
+	public String XVplusX() {
+		String first = "XV";
+		String second = "X";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String XVplusXV() {
+		String first = "XV";
+		String second = "XV";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+
+	public String XXplusI() {
+		String first = "XX";
+		String second = "I";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+
+	public String XXIplusIV() {
+		String first = "XXI";
+		String second = "IV";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String XXIVplusXXVI() {
+		String first = "XXIV";
+		String second = "XXVI";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String XXIplusXIX() {
+		String first = "XXI";
+		String second = "XIX";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String XXXplusXIV() {
+		String first = "XXX";
+		String second = "XIV";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String XLIVplusLII() {
+		String first = "XLIV";
+		String second = "LII";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String XXIXplusLXXI() {
+		String first = "XXIX";
+		String second = "LXXI";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+	
+	public String DCCLIIIplusMCXLVII() {
+		String first = "DCCLIII";
+		String second = "MCXLVII";
+		
+		first = toIs(first);
+		second = toIs(second);
+		
+		String sum = (first + second);
+		
+		String result = toRomans(sum);
+		
+		return result;
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
